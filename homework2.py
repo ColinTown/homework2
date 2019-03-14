50611b60e8c3220fcd5608584fee5489fb945b89
Colin Town <Colin_Town@Yahoo.com>
2019-03-14 05:12:20 -0500

    Added less than functionality

--- homework2.py
+++ homework2.py
@@ -3,4 +3,5 @@ number2 = float(input("Please enter the first number "))
 
 if number1 > number2:
     print("num 1 is bigger than num 2")
-
+if number1 < number2:
+    print("num 2 is bigger than num 1")
