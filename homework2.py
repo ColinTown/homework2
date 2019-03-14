01bbafb0be288f662417161a16031a4aeeb57123
Colin Town <Colin_Town@Yahoo.com>
2019-03-14 05:14:15 -0500

    Fixed grammatical errors

---
+++ homework2.py
@@ -0,0 +1,7 @@
+number1 = float(input("Please enter the first number "))
+number2 = float(input("Please enter the first number "))
+
+if number1 > number2:
+    print("Number 1 is bigger than Number 2")
+if number1 < number2:
+    print("Number 2 is bigger than Number 1")
